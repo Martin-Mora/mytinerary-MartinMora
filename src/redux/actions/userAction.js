84ba2ee2661198178f811413f3c5ca03a8b07c45
Martin Nicolas Mora Carvallo <94276240+Martin-Mora@users.noreply.github.com>
2024-02-14 01:39:33 -0300

    Update userAction.js path back

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -9,7 +9,7 @@ export const userRegister = createAction( 'user_register', (user) => {
 
 export const signUp = createAsyncThunk( "create_user", async ( body ) => {
   try {
-      const response = await axios.post( 'https://mytinerary-back-martinmora-1c1h-dev.fl0.io/api/verification/signup', body )
+      const response = await axios.post( 'https://mytinerary-back-martinmora-production.up.railway.app/api/verification/signup', body )
       localStorage.setItem( 'token', response.data.token )
       console.log(response.data.token);
       return response.data
@@ -20,7 +20,7 @@ export const signUp = createAsyncThunk( "create_user", async ( body ) => {
 
 export const signIn = createAsyncThunk( "log_user", async ( body ) => {
   try {
-      const response = await axios.post( 'https://mytinerary-back-martinmora-1c1h-dev.fl0.io/api/verification/signin', body )
+      const response = await axios.post( 'https://mytinerary-back-martinmora-production.up.railway.app/api/verification/signin', body )
       localStorage.setItem( 'token', response.data.token )
       console.log(response.data.token);
       console.log(response);
@@ -42,7 +42,7 @@ export const logOut = createAction( "log_out", async () => {
 export const signInToken = createAsyncThunk( "token_login", async (  ) => {
   try {
       const token = localStorage.getItem( 'token' )
-      const response = await axios.post( 'https://mytinerary-back-martinmora-1c1h-dev.fl0.io/api/verification/signin/token',{}, {
+      const response = await axios.post( 'https://mytinerary-back-martinmora-production.up.railway.app/api/verification/signin/token',{}, {
           headers: {
               Authorization : "Bearer " + token
             }
