c253fd41b1fcb824106c1bb7bbd8c415829e1cda
Martin Mora <martin_mora1209@hotmail.com>
2026-02-05 23:09:02 -0300

    CityDetails limpio y listo para producción, Node 24

--- src/components/CityDetails/CityDetails.jsx
+++ src/components/CityDetails/CityDetails.jsx
@@ -1,82 +1,89 @@
 import { useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-
 import "../CityDetails/cityDetails.css";
 import "../TineraryCard/tineraryCard.css";
-
+import { Link, useParams } from "react-router-dom";
 import TineraryCard from "../TineraryCard/TineraryCard";
 import UnderConstruction from "../UnderConstruction/UnderConstruction";
 import Activities from "../Activities/Activities";
 import SpinnerLoading from "../SpinnerLoading/SpinnerLoading";
-
-import { get_city, get_tineraries } from "../../redux/actions/cityAction.js";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  get_city,
+  resetCity,
+  get_tineraries, 
+  city_render,
+} from "../../redux/actions/cityAction.js";
 
 const CityDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
 
-  // Traer datos al montar o cambiar de ciudad
   useEffect(() => {
+    dispatch(city_render());
     dispatch(get_city(id));
     dispatch(get_tineraries());
-  }, [id, dispatch]);
+    return () => dispatch(resetCity());
+  }, [id, dispatch]); 
 
-  // Estado global de Redux
-  const {
-    city: cityStore,
-    tineraries: tinerariesStore = [], // aseguramos que siempre sea array
-    loadingCity,
-    loadingTineraries,
-  } = useSelector((store) => store.cityReducer);
+  // Protecciones contra undefined
+  const cityStore = useSelector((store) => store.cityReducer.city) || null;
+  const tinerariesStore = useSelector((store) => store.cityReducer.tineraries) || [];
 
-  // Spinner mientras carga
-  if (loadingCity || loadingTineraries || !cityStore) {
-    return <SpinnerLoading />;
-  }
+  // Filtrar tinerarios que pertenecen a la ciudad actual
+  const renderTinerary = tinerariesStore.filter(
+    (tinerary) => cityStore && tinerary.city === cityStore._id
+  );
 
-  // Itinerarios de la ciudad actual
-  const cityItineraries = tinerariesStore.filter(
-    (tinerary) => tinerary.city === cityStore._id
+  // Mostrar tinerarios
+  const showTinerary =
+    renderTinerary.length > 0 ? (
+      renderTinerary.map((tinerary) => (
+        <div key={tinerary._id} className="containerTinerary">
+          <TineraryCard value={tinerary} />
+          <Activities />
+        </div>
+      ))
+    ) : (
+      <UnderConstruction />
+    );
+
+  // Información de la ciudad
+  const renderCityInfo = (
+    <>
+      {cityStore && cityStore.city && (
+        <h2 className="containerCity__infoCity">{cityStore.city}</h2>
+      )}
+      {cityStore && cityStore.description && <p>{cityStore.description}</p>}
+    </>
   );
 
   return (
-    <main className="containerCity">
-      {/* HERO */}
-      <div className="containerCity__info">
-        <div className="overlay"></div>
-        <img src={cityStore.image} alt={cityStore.city} />
-
-        <div className="containerCity__general">
-          <h2 className="containerCity__infoCity">{cityStore.city}</h2>
-          <p>{cityStore.description}</p>
-
-          <a href="#itineraries">
-            <button className="containerCity__btnCity">View More ↓</button>
-          </a>
-
-          <Link to="/cities">
-            <button className="containerCity__btnBack">Go Back</button>
-          </Link>
-        </div>
-      </div>
-
-      {/* ITINERARIES */}
-      <section className="itineraries" id="itineraries">
-        <h2 className="tinerary-title">Itineraries</h2>
-
-        {cityItineraries.length > 0 ? (
-          cityItineraries.map((tinerary) => (
-            <div key={tinerary._id} className="containerTinerary">
-              <TineraryCard value={tinerary} />
-              <Activities />
+    <>
+      {cityStore ? (
+        <main className="containerCity">
+          <div className="containerCity__info">
+            <div className="overlay"></div>
+            <img src={cityStore.image} alt={cityStore.city || "City Image"} />
+            <div className="containerCity__general">
+              {renderCityInfo}
+              <a href="#itineraries">
+                <button className="containerCity__btnCity">View More ↓</button>
+              </a>
+              <Link to="/cities/">
+                <button className="containerCity__btnBack">Go Back</button>
+              </Link>
             </div>
-          ))
-        ) : (
-          <UnderConstruction />
-        )}
-      </section>
-    </main>
+          </div>
+
+          <section className="itineraries" id="itineraries">
+            <h2 className="tinerary-title">Itineraries</h2>
+            {showTinerary}
+          </section>
+        </main>
+      ) : (
+        <SpinnerLoading />
+      )}
+    </>
   );
 };
 
